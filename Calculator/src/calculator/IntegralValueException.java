29cf5fa5352c0c51219f8da22b8aee227574506f
MaxSDemin <maximsd3@gmail.com>
2025-03-05 09:11:12 +0300

    report lab3

---
+++ Calculator/src/calculator/IntegralValueException.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class IntegralValueException extends Exception {
+    public IntegralValueException(double lowerBound, double upperBound){
+        String message = String.format(
+                "Value exception: lower bound - %f, upper bound - %f",
+                lowerBound, upperBound);
+        super(message);
+    }
+    
+    public String getExceptionName(){
+        return "IntegralValueException";
+    }
+}
